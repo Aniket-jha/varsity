fa15ac8d85e4e02c62337a68de823ff2b9ba718d
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-01-22 20:41:20 +0530

    Changed Brochure Link

--- src/Components/Rooms.js
+++ src/Components/Rooms.js
@@ -14,7 +14,7 @@ const Rooms = () => {
                    
                 </div>
                 <div className='lg:flex hidden  mt-2 lg:mt-3 items-center justify-center' >
-           <a target='_blank' rel='noreferrer' href="https://firebasestorage.googleapis.com/v0/b/union-living.appspot.com/o/files%2FVarsity%20by%20Union%20Digital.pdf?alt=media&token=5be22970-7bfe-40ff-9a07-64e4800b2f03" download> <button className="uppercase flex items-center justify-center gap-2 lg:gap-3 rounded-lg shadow-md  font-[NeueMedium] ease-in duration-300  text-[12px] md:text-[16px]  hover:shadow-none hover:bg-[#272727] tracking-wider py-[12px] px-[16px] mt-3 md:py-[12px] md:px-[24px] text-[#FFFEFC] bg-[#D64C27]" > What's Included in Rent?  </button></a>
+           <a target='_blank' rel='noreferrer' href="https://firebasestorage.googleapis.com/v0/b/grc-logistics.appspot.com/o/files%2Fbrochure.pdf?alt=media&token=ea846c57-a320-492f-af9b-6dbf8de7b5e2" download> <button className="uppercase flex items-center justify-center gap-2 lg:gap-3 rounded-lg shadow-md  font-[NeueMedium] ease-in duration-300  text-[12px] md:text-[16px]  hover:shadow-none hover:bg-[#272727] tracking-wider py-[12px] px-[16px] mt-3 md:py-[12px] md:px-[24px] text-[#FFFEFC] bg-[#D64C27]" > What's Included in Rent?  </button></a>
            </div>
             </div>
     </div>
